4dd1ca41a263673cac623a25b0b8e092d6912982
i252-hub <jushirarey@gmail.com>
2025-05-10 11:38:54 +0800

    revision

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ tailwindcss(),
-            react()],
+            react(),
+            svgr()],
 })
